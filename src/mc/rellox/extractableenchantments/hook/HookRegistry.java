63ceae41c20e2a620c01607a210262784067e274
tbm00 <89701900+tbm00@users.noreply.github.com>
2025-05-04 23:57:40 -0500

    Got compiling

--- src/mc/rellox/extractableenchantments/hook/HookRegistry.java
+++ src/mc/rellox/extractableenchantments/hook/HookRegistry.java
@@ -14,23 +14,9 @@ public final class HookRegistry {
 	private static final Map<String, IHook> HOOKS = new HashMap<>();
 	
 	public static final EconomyHook economy = new EconomyHook();
-	public static final EcoEnchantsHook eco_enchants = new EcoEnchantsHook();
-	public static final ExcellentEnchantsHook excellent_enchants = new ExcellentEnchantsHook();
-	public static final VaneHook vane = new VaneHook();
-	public static final EnchantmentPlusHook enchantments_plus = new EnchantmentPlusHook();
-	public static final AdvancedEnchantmentsHook advanced_enchantments = new AdvancedEnchantmentsHook();
-	public static final EnchantsSquaredHook enchants_squared = new EnchantsSquaredHook();
-	public static final CrazyEnchantmentsHook crazy_enchantments = new CrazyEnchantmentsHook();
 	
 	static {
 		HOOKS.put(economy.name(), economy);
-		HOOKS.put(eco_enchants.name(), eco_enchants);
-		HOOKS.put(excellent_enchants.name(), excellent_enchants);
-		HOOKS.put(vane.name(), vane);
-		HOOKS.put(enchantments_plus.name(), enchantments_plus);
-		HOOKS.put(advanced_enchantments.name(), advanced_enchantments);
-		HOOKS.put(enchants_squared.name(), enchants_squared);
-		HOOKS.put(crazy_enchantments.name(), crazy_enchantments);
 	}
 	
 	public static void initialize() {
